a42558eaf1660de9aba2f976a893666c830bef47
itosho <sho.ito@supership.jp>
2017-04-23 23:50:40 +0900

    add send slack feature

--- webhook.go
+++ webhook.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/labstack/echo"
@@ -69,4 +73,31 @@ func postMessage(c echo.Context) error {
 		}
 	}
 	return c.JSON(http.StatusOK, "success")
+}
+
+func sendToSlack(path string, text string) (string, error) {
+	slackURL := "https://hooks.slack.com"
+	slackPath := path
+	u, _ := url.ParseRequestURI(slackURL)
+	u.Path = slackPath
+
+	urlStr := fmt.Sprintf("%v", u)
+
+	data := url.Values{}
+	data.Set("payload", "{\"text\": \""+text+"\", \"link_names\": 1}")
+
+	client := &http.Client{}
+	req, _ := http.NewRequest("POST", urlStr, strings.NewReader(data.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	res, err := client.Do(req)
+
+	defer res.Body.Close()
+
+	b, _ := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		return string(b), err
+	}
+	return string(b), nil
 }
